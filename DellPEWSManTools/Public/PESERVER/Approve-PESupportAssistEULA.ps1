a2071a51695fa9df70751a21ba44fff9a0dc41aa
Doug Roorda <droorda@iconicgroup.com>
2020-06-30 16:24:50 -0400

    Change Accept-PESupportAssistEULA to approved Verb

--- DellPEWSManTools/Public/PESERVER/Approve-PESupportAssistEULA.ps1
+++ DellPEWSManTools/Public/PESERVER/Approve-PESupportAssistEULA.ps1
@@ -1,5 +1,5 @@
 <#
-Accept-PESupportAssistEULA.ps1 - Accept EULA terms and conditions required for support assist registration and running supportassistcollection.
+Approve-PESupportAssistEULA.ps1 - Accept EULA terms and conditions required for support assist registration and running supportassistcollection.
 
 _author_ = Kristian Lamb <Kristian.Lamb@Dell.com> _version_ = 1.0
 
@@ -7,7 +7,7 @@ Copyright (c) 2019, Dell, Inc.
 
 This software is licensed to you under the GNU General Public License, version 2 (GPLv2). There is NO WARRANTY for this software, express or implied, including the implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2 along with this software; if not, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #>
-function Accept-PESupportAssistEULA
+function Approve-PESupportAssistEULA
 {
     [CmdletBinding(DefaultParameterSetName='General')]
     Param
