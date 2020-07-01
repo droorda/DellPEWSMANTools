84d25393371dcd1509beab7e5c9653861734c07c
Doug Roorda <droorda@iconicgroup.com>
2020-07-01 16:23:06 -0400

    indention cleanup

--- DellPEWSManTools/Public/PESERVER/Set-PESupportAssistAutoCollectSchedule.ps1
+++ DellPEWSManTools/Public/PESERVER/Set-PESupportAssistAutoCollectSchedule.ps1
@@ -9,75 +9,75 @@ This software is licensed to you under the GNU General Public License, version 2
 #>
 function Set-PESupportAssistAutoCollectSchedule
 {
-  Param
-  (
-      [Parameter(Mandatory,
-                 ParameterSetName='Monthly')]
-      [Parameter(Mandatory,
-                 ParameterSetName='Weekly')]
-      [Alias("s")]
-      [ValidateNotNullOrEmpty()]
-      $iDRACSession,
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Low', DefaultParameterSetName='General')]
+    Param
+    (
+        [Parameter(Mandatory,
+                    ParameterSetName='Monthly')]
+        [Parameter(Mandatory,
+                    ParameterSetName='Weekly')]
+        [Alias("s")]
+        [ValidateNotNullOrEmpty()]
+        $iDRACSession,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='Monthly')]
-      [Parameter(Mandatory,
-                 ParameterSetName='Weekly')]
-      [ValidateNotNullOrEmpty()]
-      [ValidateSet('WEEKLY','MONTHLY','QUARTERLY')]
-      [String]$Recurrence,
+        [Parameter(Mandatory,
+                    ParameterSetName='Monthly')]
+        [Parameter(Mandatory,
+                    ParameterSetName='Weekly')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('WEEKLY','MONTHLY','QUARTERLY')]
+        [String]$Recurrence,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='Monthly')]
-      [Parameter(Mandatory,
-                 ParameterSetName='Weekly')]
-      [ValidateNotNullOrEmpty()]
-      [ValidatePattern('^[0-2][0-9]:[0-5][0-9]\s[AP][M]$')]
-      [String]$Time,
+        [Parameter(Mandatory,
+                    ParameterSetName='Monthly')]
+        [Parameter(Mandatory,
+                    ParameterSetName='Weekly')]
+        [ValidateNotNullOrEmpty()]
+        [ValidatePattern('^[0-2][0-9]:[0-5][0-9]\s[AP][M]$')]
+        [String]$Time,
 
-      [Parameter(ParameterSetName='Monthly')]
-      [ValidateNotNullOrEmpty()]
-      [ValidatePattern("^[1-9][1-8]$|^[1-9L]$")]
-      [String]$DayOfMonth,
+        [Parameter(ParameterSetName='Monthly')]
+        [ValidateNotNullOrEmpty()]
+        [ValidatePattern("^[1-9][1-8]$|^[1-9L]$")]
+        [String]$DayOfMonth,
 
-      [Parameter(ParameterSetName='Weekly')]
-      [ValidateNotNullOrEmpty()]
-      [ValidateSet('1','2','3','4','L')]
-      [String]$WeekOfMonth,
+        [Parameter(ParameterSetName='Weekly')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('1','2','3','4','L')]
+        [String]$WeekOfMonth,
 
-      [Parameter(ParameterSetName='Weekly')]
-      [ValidateNotNullOrEmpty()]
-      [ValidateSet('MON','TUE','WED','THU','FRI','SAT','SUN')]
-      [String]$DayOfWeek
-  )
+        [Parameter(ParameterSetName='Weekly')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('MON','TUE','WED','THU','FRI','SAT','SUN')]
+        [String]$DayOfWeek
+    )
 
-  Begin
-  {
-      $properties= @{SystemCreationClassName="DCIM_ComputerSystem";SystemName="DCIM:ComputerSystem";CreationClassName="DCIM_LCService";Name="DCIM:LCService";}
-      $instance = New-CimInstance -ClassName DCIM_LCService -Namespace root/dcim -ClientOnly -Key @($properties.keys) -Property $properties
+    Begin
+    {
+        $properties= @{SystemCreationClassName="DCIM_ComputerSystem";SystemName="DCIM:ComputerSystem";CreationClassName="DCIM_LCService";Name="DCIM:LCService";}
+        $instance = New-CimInstance -ClassName DCIM_LCService -Namespace root/dcim -ClientOnly -Key @($properties.keys) -Property $properties
 
-      $Parameters = @{
-          Time = $Time
-          Recurrence = [SupportAssistRecurrence]$Recurrence -as [int]
-      }
-      if ($DayOfMonth)
-      {
-          $Parameters.Add('DayOfMonth', $DayOfMonth)
-      }
-      if ($WeekOfMonth)
-      {
-          $Parameters.Add('WeekOfMonth', $WeekOfMonth)
-      }
-      if ($DayOfWeek)
-      {
-          $Parameters.Add('DayOfWeek', $DayOfWeek)
-      }
-  }
+        $Parameters = @{
+            Time = $Time
+            Recurrence = [SupportAssistRecurrence]$Recurrence -as [int]
+        }
+        if ($DayOfMonth)
+        {
+            $Parameters.Add('DayOfMonth', $DayOfMonth)
+        }
+        if ($WeekOfMonth)
+        {
+            $Parameters.Add('WeekOfMonth', $WeekOfMonth)
+        }
+        if ($DayOfWeek)
+        {
+            $Parameters.Add('DayOfWeek', $DayOfWeek)
+        }
+    }
 
-  Process
-  {
-      $cmdresponse = Invoke-CimMethod -InputObject $instance -MethodName SupportAssistSetAutoCollectSchedule -CimSession $iDRACSession -Arguments $Parameters
-      return $cmdresponse
-  }
+    Process
+    {
+        $cmdresponse = Invoke-CimMethod -InputObject $instance -MethodName SupportAssistSetAutoCollectSchedule -CimSession $iDRACSession -Arguments $Parameters
+        return $cmdresponse
+    }
 }
