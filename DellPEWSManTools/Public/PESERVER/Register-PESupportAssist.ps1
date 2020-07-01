7f9f885ac55e7df1b41b9f978027070556ab4f67
Doug Roorda <droorda@iconicgroup.com>
2020-07-01 16:24:14 -0400

    Update Register-PESupportAssist.ps1
    change from user/pass parameters to credentials object

--- DellPEWSManTools/Public/PESERVER/Register-PESupportAssist.ps1
+++ DellPEWSManTools/Public/PESERVER/Register-PESupportAssist.ps1
@@ -9,194 +9,201 @@ This software is licensed to you under the GNU General Public License, version 2
 #>
 function Register-PESupportAssist
 {
-  [CmdletBinding(DefaultParameterSetName='General')]
-  Param
-  (
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [Alias("s")]
-      [ValidateNotNullOrEmpty()]
-      $iDRACSession,
+    [CmdletBinding(DefaultParameterSetName='General')]
+    Param
+    (
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [Alias("s")]
+        [ValidateNotNullOrEmpty()]
+        $iDRACSession,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$PrimaryFirstName,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$PrimaryFirstName,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$PrimaryLastName,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$PrimaryLastName,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$PrimaryPhoneNumber,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$PrimaryPhoneNumber,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [mailaddress]$PrimaryEmail,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [mailaddress]$PrimaryEmail,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$CompanyName,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$CompanyName,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$Street1,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Street1,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$State,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$State,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$City,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$City,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$Zip,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Zip,
 
-      [Parameter(Mandatory,
-                 ParameterSetName='General')]
-      [ValidateNotNullOrEmpty()]
-      [String]$Country,
+        [Parameter(Mandatory,
+                    ParameterSetName='General')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Country,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$PrimaryAlternateNumber,
+        [Parameter(ParameterSetName='General')]
+        [String]$PrimaryAlternateNumber,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$Street2,
+        [Parameter(ParameterSetName='General')]
+        [String]$Street2,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$SecondaryFirstName,
+        [Parameter(ParameterSetName='General')]
+        [String]$SecondaryFirstName,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$SecondaryLastName,
+        [Parameter(ParameterSetName='General')]
+        [String]$SecondaryLastName,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$SecondaryPhoneNumber,
+        [Parameter(ParameterSetName='General')]
+        [String]$SecondaryPhoneNumber,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$SecondaryAlternateNumber,
+        [Parameter(ParameterSetName='General')]
+        [String]$SecondaryAlternateNumber,
 
-      [Parameter(ParameterSetName='General')]
-      [mailaddress]$SecondaryEmail,
+        [Parameter(ParameterSetName='General')]
+        [mailaddress]$SecondaryEmail,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$ProxyHostName,
+        [Parameter(ParameterSetName='General')]
+        [String]$ProxyHostName,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$ProxyUserName,
+        [Parameter(ParameterSetName='General')]
+        [PSCredential]$ProxyCredential,
 
-      [Parameter(ParameterSetName='General')]
-      [int]$ProxyPort,
+        # [Parameter(ParameterSetName='General')]
+        # [String]$ProxyUserName,
 
-      [Parameter(ParameterSetName='General')]
-      [String]$ProxyPassword,
+        # [Parameter(ParameterSetName='General')]
+        # [String]$ProxyPassword,
 
-      [Parameter(ParameterSetName='General')]
-      [Switch]$Passthru,
+        [Parameter(ParameterSetName='General')]
+        [int]$ProxyPort,
 
-      [Parameter(ParameterSetName='General')]
-      [Switch]$Wait
-  )
+        [Parameter(ParameterSetName='General')]
+        [Switch]$Passthru,
 
-  Begin
-  {
-      $properties= @{SystemCreationClassName="DCIM_ComputerSystem";SystemName="DCIM:ComputerSystem";CreationClassName="DCIM_LCService";Name="DCIM:LCService";}
-      $instance = New-CimInstance -ClassName DCIM_LCService -Namespace root/dcim -ClientOnly -Key @($properties.keys) -Property $properties
-      $Parameters = @{
-          PrimaryFirstName = $PrimaryFirstName
-          PrimaryLastName = $PrimaryLastName
-          PrimaryPhoneNumber  = $PrimaryPhoneNumber
-          PrimaryEmail  = $PrimaryEmail.ToString()
-          CompanyName = $CompanyName
-          Street1 = $Street1
-          State = $State
-          City = $City
-          Zip = $Zip
-          Country = $Country
-      }
-      if ($PrimaryAlternateNumber)
-      {
-          $Parameters.Add('PrimaryAlternateNumber', $PrimaryAlternateNumber)
-      }
-      if ($Street2)
-      {
-          $Parameters.Add('Street2', $Street2)
-      }
-      if ($SecondaryFirstName)
-      {
-          $Parameters.Add('SecondaryFirstName', $SecondaryFirstName)
-      }
-      if ($SecondaryLastName)
-      {
-          $Parameters.Add('SecondaryLastName', $SecondaryLastName)
-      }
-      if ($SecondaryPhoneNumber )
-      {
-          $Parameters.Add('SecondaryPhoneNumber', $SecondaryPhoneNumber)
-      }
-      if ($SecondaryAlternateNumber)
-      {
-          $Parameters.Add('SecondaryAlternateNumber', $SecondaryAlternateNumber)
-      }
-      if ($SecondaryEmail)
-      {
-          $Parameters.Add('SecondaryEmail', $SecondaryEmail.ToString())
-      }
-      if ($ProxyHostName)
-      {
-          $Parameters.Add('ProxyHostName', $ProxyHostName)
-      }
-      if ($ProxyUserName)
-      {
-          $Parameters.Add('ProxyUserName', $ProxyUserName)
-      }
-      if ($ProxyPort)
-      {
-          $Parameters.Add('ProxyPort', $ProxyPort)
-      }
-      if ($ProxyPassword)
-      {
-          $Parameters.Add('ProxyPassword', $ProxyPassword)
-      }
-  }
+        [Parameter(ParameterSetName='General')]
+        [Switch]$Wait
+    )
 
-  Process
-  {
+    Begin
+    {
+        $properties= @{SystemCreationClassName="DCIM_ComputerSystem";SystemName="DCIM:ComputerSystem";CreationClassName="DCIM_LCService";Name="DCIM:LCService";}
+        $instance = New-CimInstance -ClassName DCIM_LCService -Namespace root/dcim -ClientOnly -Key @($properties.keys) -Property $properties
+        $Parameters = @{
+            PrimaryFirstName = $PrimaryFirstName
+            PrimaryLastName = $PrimaryLastName
+            PrimaryPhoneNumber  = $PrimaryPhoneNumber
+            PrimaryEmail  = $PrimaryEmail.ToString()
+            CompanyName = $CompanyName
+            Street1 = $Street1
+            State = $State
+            City = $City
+            Zip = $Zip
+            Country = $Country
+        }
+        if ($PrimaryAlternateNumber)
+        {
+            $Parameters.Add('PrimaryAlternateNumber', $PrimaryAlternateNumber)
+        }
+        if ($Street2)
+        {
+            $Parameters.Add('Street2', $Street2)
+        }
+        if ($SecondaryFirstName)
+        {
+            $Parameters.Add('SecondaryFirstName', $SecondaryFirstName)
+        }
+        if ($SecondaryLastName)
+        {
+            $Parameters.Add('SecondaryLastName', $SecondaryLastName)
+        }
+        if ($SecondaryPhoneNumber )
+        {
+            $Parameters.Add('SecondaryPhoneNumber', $SecondaryPhoneNumber)
+        }
+        if ($SecondaryAlternateNumber)
+        {
+            $Parameters.Add('SecondaryAlternateNumber', $SecondaryAlternateNumber)
+        }
+        if ($SecondaryEmail)
+        {
+            $Parameters.Add('SecondaryEmail', $SecondaryEmail.ToString())
+        }
+        if ($ProxyHostName)
+        {
+            $Parameters.Add('ProxyHostName', $ProxyHostName)
+        }
+        # if ($ProxyUserName)
+        # {
+        #     $Parameters.Add('ProxyUserName', $ProxyUserName)
+        # }
+        if ($ProxyPort)
+        {
+            $Parameters.Add('ProxyPort', $ProxyPort)
+        }
+        # if ($ProxyPassword)
+        # {
+        #     $Parameters.Add('ProxyPassword', $ProxyPassword)
+        # }
+        if ($ProxyCredential){
+            $Parameters.Add('ProxyUserName', $ProxyCredential.UserName)
+            $Parameters.Add('ProxyPassword', $ProxyCredential.GetNetworkCredential().password)
+        }
+    }
 
-      $job = Invoke-CimMethod -InputObject $instance -MethodName SupportAssistRegister -CimSession $iDRACSession -Arguments $Parameters
+    Process
+    {
 
-      if ($job.ReturnValue -eq 4096)
-      {
-          if ($Passthru)
-          {
-              $job
-          }
-          elseif ($Wait)
-          {
-              Wait-PEConfigurationJob -iDRACSession $iDRACSession -JobID $job.Job.EndpointReference.InstanceID -Activity "Register SupportAssist for $($iDRACSession.ComputerName)"
-          }
-          else
-          {
-              return $job
-          }
-      }
-      elseif ($job.ReturnValue -eq 0)
-      {
-          return $job
-      }
-      else
-      {
-          Throw "Job creation failed with an error: $($Job.Message)"
-      }
-  }
+        $job = Invoke-CimMethod -InputObject $instance -MethodName SupportAssistRegister -CimSession $iDRACSession -Arguments $Parameters
+
+        if ($job.ReturnValue -eq 4096)
+        {
+            if ($Passthru)
+            {
+                $job
+            }
+            elseif ($Wait)
+            {
+                Wait-PEConfigurationJob -iDRACSession $iDRACSession -JobID $job.Job.EndpointReference.InstanceID -Activity "Register SupportAssist for $($iDRACSession.ComputerName)"
+            }
+            else
+            {
+                return $job
+            }
+        }
+        elseif ($job.ReturnValue -eq 0)
+        {
+            return $job
+        }
+        else
+        {
+            Throw "Job creation failed with an error: $($Job.Message)"
+        }
+    }
 }
