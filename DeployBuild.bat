9b8a53643bba710e3a1d092a562f090708a3bf58
Doug Roorda <droorda@iconicgroup.com>
2020-10-07 12:54:26 -0400

    Update DeployBuild.bat

--- DeployBuild.bat
+++ DeployBuild.bat
@@ -3,6 +3,5 @@
 pushd %~dp0
 
 powershell.exe -inputformat none -ExecutionPolicy Bypass -NonInteractive -command "& .\Build\Start-Build.ps1 %*"
-REM powershell.exe -inputformat none -ExecutionPolicy Bypass -noprofile -NonInteractive -command "& .\Build\Start-Build.ps1 %*"
 
 popd
