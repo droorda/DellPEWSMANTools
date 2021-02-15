c6a276faf980c38d50ecc68102c67fa50705835c
Doug Roorda <droorda@iconicgroup.com>
2021-02-15 10:42:28 -0500

    Update New-PEDRACSession.ps1

--- DellPEWSManTools/Public/PEDRAC/New-PEDRACSession.ps1
+++ DellPEWSManTools/Public/PEDRAC/New-PEDRACSession.ps1
@@ -16,23 +16,23 @@ function New-PEDRACSession
         [Parameter (Mandatory)]
         [PSCredential]
         [System.Management.Automation.Credential()]
-        $Credential,
-
+        $Credential
+        ,
         [Parameter (Mandatory)]
         [Alias("IPAddress","HostName")]
-        [string] $ComputerName,
-
-        [string] $Authentication = "Basic",
-
-        [int]    $Port = 443,
-
-        [string] $Encoding = 'Utf8',
-
-        [bool]   $UseSsl = $true,
-
+        [string] $ComputerName
+        ,
+        [string] $Authentication = "Basic"
+        ,
+        [int]    $Port = 443
+        ,
+        [string] $Encoding = 'Utf8'
+        ,
+        [bool]   $UseSsl = $true
+        ,
         [Alias("MaxTimeout")]
-        [int]    $OperationTimeoutSec = 60,
-
+        [int]    $OperationTimeoutSec = 60
+        ,
         [switch] $IgnoreCertFailures
         )
 
@@ -42,8 +42,7 @@ function New-PEDRACSession
             Encoding = $Encoding
             UseSsl   = $UseSsl
         }
-        if (([System.Net.IPAddress]::TryParse($ComputerName,[ref]$null)) -or $IgnoreCertFailures)
-        {
+        if (([System.Net.IPAddress]::TryParse($ComputerName,[ref]$null)) -or $IgnoreCertFailures) {
             $Params.SkipCACheck = $true
             $Params.SkipCNCheck = $true
             $Params.SkipRevocationCheck = $true
@@ -63,21 +62,17 @@ function New-PEDRACSession
                 $session = New-CimSession -Authentication Basic -Credential $Credential -ComputerName $ComputerName -Port 443 -SessionOption $cimOptions -OperationTimeoutSec $OperationTimeoutSec -ErrorAction Stop
             } catch {
                 try {
+                    Write-Verbose $_.exception.message
+                    $cimOptions | out-string | write-Verbose
                     Start-Sleep -s 10
                     $session = New-CimSession -Authentication Basic -Credential $Credential -ComputerName $ComputerName -Port 443 -SessionOption $cimOptions -OperationTimeoutSec $OperationTimeoutSec -ErrorAction Stop
                 } catch {
                     try {
+                        Write-Verbose $_.exception.message
                         Start-Sleep -s 60
                         $session = New-CimSession -Authentication Basic -Credential $Credential -ComputerName $ComputerName -Port 443 -SessionOption $cimOptions -OperationTimeoutSec $OperationTimeoutSec -ErrorAction Stop
                     } catch {
-                        $PSCmdlet.WriteError([System.Management.Automation.ErrorRecord]::new(
-                            ([Exception]::new("New-PEDRACSession Failed : $($_.Exception.Message)")),
-                            "1",
-                            [System.Management.Automation.ErrorCategory]::NotSpecified,
-                            $null # $TargetObject # usually the object that triggered the error, if possible
-                        ))
-                        return
-
+                        Throw "New-PEDRACSession Failed : $($_.Exception.Message)"
                     }
                 }
             }
