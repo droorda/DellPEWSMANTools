d0f1dbc9ab1277f8e3fa41114791f3c771086e01
Doug Roorda <droorda@iconicgroup.com>
2025-02-10 16:48:34 -0500

    Update DeployBuild.bat

--- DeployBuild.bat
+++ DeployBuild.bat
@@ -4,21 +4,17 @@ pushd %~dp0
 
 for /f "delims=" %%i in ('git branch --show-current') do set Branch=%%i
 IF /i "%Branch%"=="master" goto ReleaseStandard
-
-for /f "delims=" %%i in ('git branch --show-current') do set Branch=%%i
 IF /i "%Branch%"=="main" goto ReleaseStandard
-
-for /f "delims=" %%i in ('git branch --show-current') do set Branch=%%i
 IF /i "%Branch%"=="local" goto ReleaseStandard
-
-for /f "delims=" %%i in ('git branch --show-current') do set Branch=%%i
 IF /i "%Branch%"=="internal" goto ReleaseStandard
 
 :ReleaseBeta
+echo "Beta Build"
 powershell.exe -inputformat none -ExecutionPolicy Bypass -NonInteractive -command "& .\Build\Start-Build.ps1 -Beta %*"
 goto commonexit
 
 :ReleaseStandard
+echo "Standard Build"
 powershell.exe -inputformat none -ExecutionPolicy Bypass -NonInteractive -command "& .\Build\Start-Build.ps1 %*"
 goto commonexit
 
