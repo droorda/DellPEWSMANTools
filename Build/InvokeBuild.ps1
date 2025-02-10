01ce3830fc395a05e2dc185582c12f30387722fc
Doug Roorda <droorda@iconicgroup.com>
2025-02-10 14:37:24 -0500

    Build Process Changes

--- Build/InvokeBuild.ps1
+++ Build/InvokeBuild.ps1
@@ -56,17 +56,17 @@ task Test Init, {
         if (Test-Path -Path "$ProjectRoot\Tests\$TestType")
         {
             # Gather test results. Store them in a variable and file
-            $TestResults = Invoke-Pester -Path "$ProjectRoot\Tests\$TestType" -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
+            $TestResults = Invoke-Pester -Path "$ProjectRoot\Tests\$TestType" -PassThru #-OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
 
             # In Appveyor?  Upload our tests! #Abstract this into a function?
-            If($ENV:BHBuildSystem -eq 'AppVeyor')
-            {
-                (New-Object 'System.Net.WebClient').UploadFile(
-                    "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-                    "$ProjectRoot\$TestFile" )
-            }
+            # If($ENV:BHBuildSystem -eq 'AppVeyor')
+            # {
+            #     (New-Object 'System.Net.WebClient').UploadFile(
+            #         "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+            #         "$ProjectRoot\$TestFile" )
+            # }
 
-            Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+            # Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
 
             # Failed tests?
             # Need to tell psake or it will proceed to the deployment. Danger!
@@ -92,18 +92,31 @@ task Build Test, {
     {
         [Version]$Version = Get-NextNugetPackageVersion -Name $env:BHProjectName -PackageSourceUrl 'https://NuGET.dev.iconic-it.com/Nuget' -ErrorAction Stop
         [Version]$LocalVersion  = Get-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion
-        $Build    = if($Version.Build    -lt 0) { 0 } else { $Version.Build + 1 }
-        if ($LocalVersion.Build -gt $Version.Build) {$Build = $LocalVersion.Build}
-        $Revision = if($Version.Revision -lt 0) { 1 } else { $LocalVersion.Revision + 1 }
-        if ($LocalVersion -ge $Version) {
-            $Version = $LocalVersion
+        # Local Version -1 to correct for failed/Beta builds
+        $VerMajor    = [Math]::MAX($Version.Major    ,($LocalVersion.Major - 1))
+        $VerMinor    = [Math]::MAX($Version.Minor    ,($LocalVersion.Minor - 1))
+
+        $VerBuild    = [Math]::MAX($Version.Build + 1,($LocalVersion.Build    ))
+        $VerBuild    = [Math]::MAX(0,$VerBuild)
+        # What is Next revision for local / deployed already incremented by function
+        $VerRevision = [Math]::MAX($Version.Revision,$LocalVersion.Revision + 1)
+        $VerRevision = [Math]::MAX(1,$VerRevision)
+
+        if ($Major) {
+            $VerMajor = $VerMajor + 1
+            $VerMinor = 0
+            $VerBuild = 0
+        } elseif ($Minor) {
+            $VerMinor = $VerMinor + 1
+            $VerBuild = 0
         }
-        $Version  = New-Object System.Version ($Version.Major, $Version.Minor, $Build, $Revision)
+
+        $env:Version  = New-Object System.Version ($VerMajor, $VerMinor, $VerBuild, $VerRevision)
         # write-Verbose "BHProjectName      - $env:BHProjectName" -Verbose
-        write-Verbose "Version            - $Version" -Verbose
+        write-Verbose "Version            - $env:Version" -Verbose
         # write-Verbose "BHPSModuleManifest - $env:BHPSModuleManifest" -Verbose
 
-        Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $Version -ErrorAction stop
+        Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $env:Version -ErrorAction stop
     }
     Catch
     {
@@ -116,14 +129,86 @@ task Deploy Build, {
 
     $Params = @{
         Path = $ProjectRoot
-        Force = $true
-        Recurse = $false # We keep psdeploy artifacts, avoid deploying those : )
-        Beta = $true
-        FeedUrl = $Credentials.NuGet.FeedUrl
-        ApiKey = $Credentials.NuGet.ApiKey
-
+        # Force = $true
+        # Recurse = $false # We keep psdeploy artifacts, avoid deploying those : )
+        FeedUrl = $env:FeedUrl
+        ApiKey = $env:ApiKey
     }
-    . C:\Projects\GitHub\PSPostMan\PSPostMan.ps1
-    Invoke-PSDeploy @Verbose @Params
-}
+    if ($env:Beta) {
+        $Params.Beta = $true
+    }
 
+    . C:\Projects\droorda\PSPostMan\PSPostMan.ps1
+    Invoke-PSDeploy @Verbose @Params
+
+    Write-Host "Checking $Repository" -ForegroundColor Cyan
+    $GitStatus = Get-GitStatus
+    if ($GitStatus.AheadBy -gt 0) {
+        Write-Host "  Pushing changes" -ForegroundColor Magenta
+        git push #| Write-Verbose
+    }
+    if ($GitStatus.BehindBy -gt 0) {
+        Write-Host "Your branch is behind '$($GitStatus.Upstream)' by $($GitStatus.BehindBy) commits" -ForegroundColor Magenta
+        if ($GitStatus.Working) {
+            Write-Host "  Stashing current changes" -ForegroundColor Magenta
+            git stash save "Automated Stash during pull" --include-untracked #| Write-Verbose
+            Write-Host "  Pulling branch changes" -ForegroundColor Magenta
+            git pull #| Write-Verbose
+            # git stash list # By default, git stash pop will re-apply the most recently created stash: stash@{0}
+            Write-Host "  Re-applying stashed changes" -ForegroundColor Magenta
+            git stash pop #| Write-Verbose
+            Start-Sleep -Seconds 1
+        } else {
+            Write-Host "  Pulling branch changes" -ForegroundColor Magenta
+            git pull #| Write-Verbose
+        }
+        $GitStatus = Get-GitStatus
+    }
+    git add $env:BHPSModuleManifest
+    Start-Sleep -Seconds 1
+    $BuildVersion = $env:Version
+    if ($env:Beta) {
+        $BuildVersion = "$BuildVersion-Beta"
+    }
+    $commitTitle = "Build Version $BuildVersion"
+    $commitDescription = "[$env:computername] $((Get-Date).ToString('yyyy:MM:dd-HH:mm:ss'))"
+    Write-Host "  Commiting '$commitTitle'" -ForegroundColor Magenta
+    git commit -m $commitTitle -m $commitDescription
+    git tag "Build-$BuildVersion"
+    $GitStatus = Get-GitStatus
+    if ($GitStatus.AheadBy -gt 0) {
+        Write-Host "  Pushing changes" -ForegroundColor Magenta
+        git push #| Write-Verbose
+    }
+
+
+    # Write-Host "Creating Nuget package" -ForegroundColor Cyan
+    # $ModulePackage = New-PMModulePackage -Verbose -PassThru -Path "$ProjectRoot\$(split-path $ProjectRoot -Leaf)"
+    # # $ModulePackage = Move-Item -path $ModulePackage -Destination "$ProjectRoot\Builds" -PassThru
+    # Write-Host "Signing Nuget package" -ForegroundColor Cyan
+    # $Certificate = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert | Where-Object {$_.NotAfter -gt (Get-date)}| Sort-Object NotAfter -Descending | Select-Object -First 1
+
+    # Set-PMPackageCert `
+    #     -path $ModulePackage.fullname `
+    #     -CertificateFingerprint $Certificate.Thumbprint `
+    #     -Timestamper 'http://sha256timestamp.ws.symantec.com/sha256/timestamp' `
+    #     -Verbose
+    # Write-Host "Publishing Nuget package" -ForegroundColor Cyan
+    # Publish-PMPackage `
+    #     -Path $ModulePackage.fullname `
+    #     -FeedUrl 'https://NuGET.dev.iconic-it.com/Nuget' `
+    #     -ApiKey '9DqH$EE3PLRT6DsW5!#3qcpq3VcJY!ZGk9Pr6ch7^XhH4mn5HKgT8pT3kpWv!7K' `
+    #     -Verbose
+    # # Get-ChildItem "$ProjectRoot\Builds" | Sort-Object Name
+    # Remove-Item -Path $ModulePackage -Force
+    # Try
+    # {
+    #     # $Version = Get-NextPSGalleryVersion -Name $env:BHProjectName -ErrorAction Stop
+    #     Update-Metadata -Path $env:BHPSModuleManifest -PropertyName FunctionsToExport -Value '*' -ErrorAction stop
+    # }
+    # Catch
+    # {
+    #     "Failed to set FunctionsToExport for '$env:BHProjectName': $_.`nContinuing with existing version"
+    # }
+
+}
